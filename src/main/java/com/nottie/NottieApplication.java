d4412c69f3c1e9384491cc42f22c41fd6230b1ce
João Vitor <jvitor.arrudal@gmail.com>
2025-03-01 01:34:51 -0300

    project initialization

---
+++ src/main/java/com/nottie/NottieApplication.java
@@ -0,0 +1,13 @@
+package com.nottie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NottieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NottieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nottie/NottieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nottie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NottieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
