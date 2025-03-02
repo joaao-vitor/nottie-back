f467fe92d49556a9c95ccd6467dee15c1ca4d3e0
João Vitor <jvitor.arrudal@gmail.com>
2025-03-01 22:42:26 -0300

    feat: initial user model

---
+++ src/main/java/com/nottie/model/User.java
@@ -0,0 +1,101 @@
+package com.nottie.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ColumnDefault("")
+    private String profileImg;
+
+    public User() {
+    }
+
+    public User(String email, String username, String password, String name, String profileImg) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.profileImg = profileImg;
+    }
+
+    public String getProfileImg() {
+        return profileImg;
+    }
+
+    public void setProfileImg(String profileImg) {
+        this.profileImg = profileImg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(profileImg, user.profileImg);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, username, password, name, profileImg);
+    }
+}
