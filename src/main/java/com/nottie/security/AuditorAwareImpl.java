4569aede084a5153db5a5e72e01ff2ea0bb29e6a
João Vitor <jvitor.arrudal@gmail.com>
2025-03-12 04:56:45 -0300

    chore(auditor): fixing AuditorAwareImpl

--- src/main/java/com/nottie/security/AuditorAwareImpl.java
+++ src/main/java/com/nottie/security/AuditorAwareImpl.java
@@ -6,11 +6,9 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
 public class AuditorAwareImpl implements AuditorAware<User> {
 
     private final UserRepository userRepository;
