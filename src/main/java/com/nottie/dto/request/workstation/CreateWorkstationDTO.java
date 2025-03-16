614683badbd724f96fbae09ee8e418c63b4f04ad
joaao-vitor <jvitor.arrudal@gmail.com>
2025-03-16 20:14:57 -0300

    feat: Validation for CreateWorkstationDTO

--- src/main/java/com/nottie/dto/request/workstation/CreateWorkstationDTO.java
+++ src/main/java/com/nottie/dto/request/workstation/CreateWorkstationDTO.java
@@ -1,6 +1,13 @@
 package com.nottie.dto.request.workstation;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateWorkstationDTO(
+        @NotNull(message = "Name can't be null")
+        @NotBlank(message = "Name can't be blank")
         String name,
+        @Size(min = 5, max = 60, message = "Username must have between 5 to 60 characters")
         String username
 ) {}
